f8a295f048829b074cf630d88feebce647a34ae5
ManishThulung <manish.thulung58@gmail.com>
2023-12-05 16:14:08 +0545

    env config and typeorm congfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { V1Controller } from './v1/v1.controller';
 import { V1Service } from './v1/v1.service';
 import { V1Module } from './v1/v1.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [V1Module],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ,
+    V1Module,
+  ],
   controllers: [AppController, V1Controller],
   providers: [AppService, V1Service],
 })
