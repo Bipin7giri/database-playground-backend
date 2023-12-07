bed7f1dfc941981fdf27e3ec1208f9fbf71c05b6
ManishThulung <manish.thulung58@gmail.com>
2023-12-07 13:10:05 +0545

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('database playground')
+    .setDescription('database playground apis')
+    .setVersion('1.0')
+    .addTag('all')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5050);
-  console.log('server started at port: 5050');
+  console.log(`server started at http://localhost:5050/api`);
 }
 bootstrap();
